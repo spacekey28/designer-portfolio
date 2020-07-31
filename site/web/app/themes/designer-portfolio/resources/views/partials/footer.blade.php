e8fde1568ec0799e6d619c36c2c19e8b15d1facb
Peter Son <peteson.me@gmail.com>
2020-07-31 23:57:12 +1200

    add footer

--- site/web/app/themes/designer-portfolio/resources/views/partials/footer.blade.php
+++ site/web/app/themes/designer-portfolio/resources/views/partials/footer.blade.php
@@ -1,5 +1,16 @@
 <footer class="content-info">
   <div class="container">
-    @php dynamic_sidebar('sidebar-footer') @endphp
+      <div class="row">
+          <div class="col">
+              <div class="footer-nav">
+                  <a href="work/" target="_self">Work</a>
+                  <a href="about/" target="_self">About Me</a>
+                  <a href="contact/" target="_self">Contact</a>
+                  <a href="resume/" target="_blank">Résumé</a>
+                  <a href="blog/" target="_self">Blog</a>
+              </div>
+          </div>
+      </div>
+      @php dynamic_sidebar('sidebar-footer') @endphp
   </div>
 </footer>
